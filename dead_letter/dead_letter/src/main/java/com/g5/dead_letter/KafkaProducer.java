42c7076c08bf86e4505cdc920eea8539d8087561
Veronica <vero_ely_vc@hotmail.com>
2025-02-23 23:57:47 -0500

    dead letter producer

--- dead_letter/dead_letter/src/main/java/com/g5/dead_letter/KafkaProducer.java
+++ dead_letter/dead_letter/src/main/java/com/g5/dead_letter/KafkaProducer.java
@@ -1,4 +1,23 @@
 package com.g5.dead_letter;
 
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Log4j2
+@Service
 public class KafkaProducer {
+    private final KafkaTemplate<String, String> kafkaTemplate;
+    private final String topic;
+
+    public KafkaProducer(KafkaTemplate<String, String> kafkaTemplate, @Value("${kafka.topic.main}") String topic) {
+        this.kafkaTemplate = kafkaTemplate;
+        this.topic = topic;
+    }
+
+    public void sendMessage(String message) {
+        kafkaTemplate.send(topic, message);
+        log.info("-------Producer:  " + message);
+    }
 }
